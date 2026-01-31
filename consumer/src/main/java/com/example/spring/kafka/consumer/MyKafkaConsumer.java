8570c77c956e00693f9f19e6fc471c15ec065b52
root <root@localhost.localdomain>
2026-01-31 02:14:32 -0500

    third commit

--- consumer/src/main/java/com/example/spring/kafka/consumer/MyKafkaConsumer.java
+++ consumer/src/main/java/com/example/spring/kafka/consumer/MyKafkaConsumer.java
@@ -1,14 +1,26 @@
 package com.example.spring.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyKafkaConsumer {
 
-    // topics には購読したいトピック名を指定します
     @KafkaListener(topics = "my-topic", groupId = "my-consumer-group")
-    public void consume(String message) {
-        System.out.println("Received message: " + message);
+    public void consume(ConsumerRecord<String, String> record) {
+        // 全ての情報が record オブジェクトに集約されています
+        String key = record.key();
+        String value = record.value();
+        int partition = record.partition();
+        long offset = record.offset();
+
+        try {
+            Thread.sleep(1500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Key: %s, Value: %s (Partition: %d, Offset: %d)%n", 
+                          key, value, partition, offset);
     }
 }
