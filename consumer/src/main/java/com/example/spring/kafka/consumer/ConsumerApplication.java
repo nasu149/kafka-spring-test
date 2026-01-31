fa9aac6f02e03e84cae9a5a60c44774f0a379785
root <root@localhost.localdomain>
2026-01-30 22:58:17 -0500

    second commit

--- consumer/src/main/java/com/example/spring/kafka/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/spring/kafka/consumer/ConsumerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
 }
